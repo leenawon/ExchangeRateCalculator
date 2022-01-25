833a857d949295560a69ac405a58c461dda8256b
limbs722 <limbs722@gmail.com>
2022-01-25 10:54:18 +0900

    Revert "Add - env, util 추가"
    This reverts commit a326756b5535cf252f83d6464e17459796827af4.

--- src/pages/first/First.js
+++ src/pages/first/First.js
@@ -1,26 +1,4 @@
 import React from "react";
-import styled from "styled-components";
-import Util from '../../utils';
-
-const API_KEY = process.env.REACT_APP_CURRENCY_KEY;
-
-const requestApi = async args => {
-  const getApi = (url, params) => {
-    let body = {
-      url: url,
-      method: 'GET',
-      data: { params, params }
-    }
-  }
-}
-
-const requestParams = {
-  url: '/convert',
-  method: 'GET',
-  params: {
-
-  }
-}
 
 export default function First() {
   return <div>first1</div>;
--- src/utils/index.js
+++
@@ -1,1 +0,0 @@
-export { default as Util } from './Util';
