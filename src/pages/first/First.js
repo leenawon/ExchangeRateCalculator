55bf4f07665c9df8c6030e65c89240d16a59d2b7
leenawon <nawon5154@gmail.com>
2022-03-16 01:21:10 +0900

    Modify - 스타일 적용 파일 이용해 기존 스타일 코드 제거하고 적용

--- src/pages/first/First.js
+++ src/pages/first/First.js
@@ -1,75 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import { FirstStyle } from '../../styles/FirstStyle';
 import { Util } from '../../utils';
 import { useFetch } from '../../hooks';
 import { useNavigate } from 'react-router';
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin: 0 auto;
-  width: 100vw;
-  height: 50vh;
-`;
-
-const Title = styled.h1`
-  font-weight: 700;
-  font-size: 24px;
-`;
-
-const SubTitle = styled.h2`
-  margin-bottom: 20px;
-  font-weight: 600;
-  font-size: 14px;
-`;
-
-const ContentText = styled.h5`
-  padding-right: 5px;
-  font-weight: 500;
-  font-size: 14px;
-  color: blue;
-`;
-
-const ReceptionCountry = styled.div`
-  display: flex;
-`;
-
-const SelectCountry = styled.select`
-  position: relative;
-  margin-top: 10px;
-  margin-left: 8px;
-  height: 23px;
-`;
-
-const InputFormWrapper = styled.form`
-  display: flex;
-  align-items: center;
-  height: 50px;
-`;
-
-const InputMoney = styled.input.attrs({ type: 'number' })`
-  margin: 8px;
-  max-width: 100px;
-  height: 23px;
-`;
-
-const LinkBoxs = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 50px;
-  width: 30vw;
-  border: none;
-`;
-
-const Link = styled.button`
-  cursor: pointer;
-  color: none;
-  &:visited {
-    text-decoration: none;
-    color: black;
-  }
-`;
+const {
+  Wrapper,
+  Title,
+  SubTitle,
+  ContentText,
+  ReceptionCountry,
+  SelectCountry,
+  InputFormWrapper,
+  InputMoney,
+  LinkBoxs,
+  Link,
+} = FirstStyle;
 
 export default function First() {
   const defParams = { url: 'live', params: null };
