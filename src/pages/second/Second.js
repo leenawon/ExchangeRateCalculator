23108c0ccad94e630a02ef8d54064f09285a120d
leenawon <nawon5154@gmail.com>
2022-03-18 00:45:33 +0900

    Modify - 두번째 계산기 스타일 파일 이용해 기존 스타일 코드 제거하고 수정

--- src/pages/second/Second.js
+++ src/pages/second/Second.js
@@ -1,126 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
 import axios from 'axios';
 import { useNavigate } from 'react-router';
+import { SecondStyle } from '../../styles/SecondStyle';
 
-const Box = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid black;
-`;
-
-const InputBox = styled.input`
-  margin: 1rem;
-  width: 8rem;
-  height: 2rem;
-`;
-
-const DropDownBox = styled.div`
-  margin: 1rem;
-  width: 8rem;
-  height: 2rem;
-`;
-
-const SelectEle = styled.div`
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 4rem;
-  height: 2rem;
-  border: 1px solid black;
-  border-bottom: ${(props) => (props.isSelected ? 'none' : '')};
-`;
-
-const DropDownEle = styled.li`
-  position: relative;
-  z-index: 1000;
-  display: flex;
-  display: ${(props) => (props.dropdown ? 'none' : '')};
-  align-items: center;
-  justify-content: center;
-  width: 4rem;
-  height: 2rem;
-  border: 1px solid black;
-  border-top: none;
-  background: white;
-  cursor: pointer;
-`;
-
-const TopBox = styled.div`
-  display: flex;
-  width: 18rem;
-`;
-
-const BottomBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin: 1rem;
-  width: 18rem;
-  height: 20rem;
-  border: 1px solid black;
-`;
-
-const TapWrapper = styled.div`
-  display: flex;
-`;
-
-const Tab = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 3rem;
-  height: 2rem;
-  border: 1px solid black;
-  cursor: pointer;
-  border-bottom: ${(props) => (props.isSelected ? 'none' : '')};
-`;
-
-const Display = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-  width: 15rem;
-  height: 15rem;
-  border: 1px solid black;
-  border-top: none;
-`;
-
-const ResultWrapper = styled.div`
-  display: flex;
-`;
-
-const ResultBox = styled.div`
-  display: flex;
-  width: 5rem;
-`;
-
-const CountryBox = styled.div`
-  width: 5rem;
-`;
-
-const LinkBoxs = styled.div`
-  display: flex;
-  width: 30vw;
-  justify-content: space-between;
-  border: none;
-  margin-top: 50px;
-`;
-
-const Link = styled.button`
-  cursor: pointer;
-  color: none;
-  &:visited {
-    text-decoration: none;
-    color: black;
-  }
-`;
-
-const DateBox = styled.div``;
+export const {
+  Box,
+  InputBox,
+  DropDownBox,
+  SelectEle,
+  DropDownEle,
+  TopBox,
+  BottomBox,
+  TapWrapper,
+  Tab,
+  Display,
+  ResultWrapper,
+  ResultBox,
+  CountryBox,
+  LinkBoxs,
+  Link,
+  DateBox,
+} = SecondStyle;
 
 export default function Second() {
   const country = ['USD', 'CAD', 'KRW', 'HKD', 'JPY', 'CNY'];
